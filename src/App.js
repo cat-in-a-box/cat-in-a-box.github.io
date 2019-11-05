10e44540819f51e0e30652d863703bdadfa28f31
Alexander <->
2019-11-05 03:41:00 +0200

    helmet fix

--- src/App.js
+++ src/App.js
@@ -1,43 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Helmet } from 'react-helmet'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Helmet } from "react-helmet";
 
-
-
-// Импортировал helmet, с помощью которого изменил page title. 
+// Импортировал helmet, с помощью которого изменил page title.
 // Насколько я понял, это самый простой способ делать это на реакте
-const PageTitle = 'Alexander Timoshuk'
-class MyComponent extends React.PureComponent {
-  render () {
-    return (
-      <>
-        <Helmet>
-          <title>{ PageTitle }</title>
-        </Helmet>
-        ...
-      </>
-    )
-  }
-}
+// Еще через него меняются атрибуты страницы в реакте
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a>Learn React</a>
       </header>
+      <Helmet>
+        <title>Alexander Timoshuk</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="A React.js application" />
+      </Helmet>
     </div>
   );
 }
