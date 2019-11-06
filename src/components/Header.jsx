ece38c9a7d3a3d49d7b5fe5021c1dc70ad264f18
cat-in-a-box <timoshuk.alexander@gmail.com>
2019-11-06 06:40:22 +0200

    fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Header = () => {
   return (
     <div className="navbar">
-        <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#portfolio">Works</a>
-        <a href="#contact">Contact</a>
+      <div className="navbarButton1"><a href="#home">Home</a></div>
+      <div className="navbarButton2"><a href="#about">About</a></div>
+      <div className="navbarButton3"><a href="#portfolio">Works</a></div>
+      <div className="navbarButton4"><a href="#contact">Contact</a></div>
     </div>
   )
 };
