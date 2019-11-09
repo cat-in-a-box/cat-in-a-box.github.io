c8f36279a420d5a1c4dce2355bda84e22202f48b
cat-in-a-box <timoshuk.alexander@gmail.com>
2019-11-09 05:11:37 +0200

    deleted unused files

--- src/components/Face.jsx
+++ src/components/Face.jsx
@@ -4,7 +4,7 @@ const Face = () => {
     return (
         <div className="Face">
             <h1>Hello. I am Alexander</h1>
-            <h2>React developer</h2>
+            <h2>Beginner React developer</h2>
             <p className="reactIcon"> <img src="react-icon.png" alt="react-icon" height="64" width="64"/></p>
         </div>
     )       
