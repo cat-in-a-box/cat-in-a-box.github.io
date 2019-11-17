9ddaa0d85047798b57ccc35378269ef353c78a2b
cat-in-a-box <timoshuk.alexander@gmail.com>
2019-11-17 10:00:27 +0200

    proper syntax, cleaner code

--- src/App.js
+++ src/App.js
@@ -6,30 +6,33 @@ import "./App.css";
 // А еще подгружает скрипты. Нормально.
 // Поставил еще React-Scrollable-Anchor для красивых якорей и плавного перемещения
 
-import { Helmet } from "react-helmet";
+import {Helmet} from "react-helmet";
 import Header from './components/Header.jsx';
 import Face from "./components/Face.jsx";
 import About from "./components/About.jsx";
 import Works from "./components/Works.jsx";
 import Footer from "./components/Footer.jsx";
 import Contact from "./components/Contact.jsx";
+
 //import ScrollButton from "./components/ScrollButton.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <Helmet>
-        <title>Alexander Timoshuk</title>
-        <meta charSet="utf-8" />
-      </Helmet>
-      <Header/>
-      <Face/>
-      <About/>
-      <Works/>
-      <Footer/>
-      <Contact/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Helmet>
+                <title>Alexander Timoshuk</title>
+                <meta charSet="utf-8"/>
+                <link rel="preload" href="App.css" as="style"/>
+                {/*прелоад шрифтов, который не работает*/}
+            </Helmet>
+            <Header/>
+            <Face/>
+            <About/>
+            <Works/>
+            <Footer/>
+            <Contact/>
+        </div>
+    );
 }
 
 export default App;
