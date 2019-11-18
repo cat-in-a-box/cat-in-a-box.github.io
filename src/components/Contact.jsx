b5071f9d9dc09baf62e272484510b78177d09573
cat-in-a-box <timoshuk.alexander@gmail.com>
2019-11-18 02:36:38 +0200

    contact anchor fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import ScrollableAnchor from 'react-scrollable-anchor';
 
 const Contact = () => {
     return (
-        <ScrollableAnchor id={'#section4'}>
+        <ScrollableAnchor id={'section4'}>
             <div className="Contact">
                 <h1>Contact me</h1>
                 <p>You can reach me me any time.</p>
