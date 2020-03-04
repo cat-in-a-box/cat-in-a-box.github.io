f47f7f7dada577a2ef17a64e833b6af67fa87851
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-03-04 07:40:41 +0200

    link change

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,7 +15,7 @@ const Works = () => {
                                 correctly</h4>
                         </div>
                     </a>
-                    <a href="https://cat-in-a-box.github.io/gallery-react/calculator" className="Card">
+                    <a href="https://cat-in-a-box.github.io/calculator" className="Card">
                         <img src={"calculator.png"} alt="CaloriesIcon"/>
                         <img src={"calculator2.png"} alt="CaloriesIcon2"/>
                         <div className="CardContainer">
