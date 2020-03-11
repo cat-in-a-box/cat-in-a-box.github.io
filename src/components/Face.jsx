0084787f4c846da7cdc18551be611700824bf296
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-03-11 18:34:54 +0200

    Update Face.jsx

--- src/components/Face.jsx
+++ src/components/Face.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const Face = () => {
     return (
         <div className="Face">
-            <h1>Hello. I am Alexander</h1>
+            <h1>Hello!</h1>
+            <h1>I am Alexander</h1>
             <h2>Beginner React developer</h2>
-            <p className="reactIcon"><img src={"react-icon.png"} alt="react-icon" height="64" width="64"/></p>
+            <img className="reactIcon" img src={"react-icon.png"} alt="react-icon" height="64" width="64"/>
         </div>
     )
 };
