0123f1890f87c347f05d3ea3dc35889460804792
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-03-11 18:30:33 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className="navbar">
-            <div className="navbarButton1"><a href={"#section1"}>Home</a></div>
-            <div className="navbarButton2"><a href={"#section2"}>About</a></div>
-            <div className="navbarButton3"><a href={"#section3"}>Works</a></div>
-            <div className="navbarButton4"><a href={"#section4"}>Contact</a></div>
+        <div className="Header">
+            <div className="HeaderButton1"><a href={"#section1"}>Home</a></div>
+            <div className="HeaderButton2"><a href={"#section2"}>About</a></div>
+            <div className="HeaderButton3"><a href={"#section3"}>Works</a></div>
+            <div className="HeaderButton4"><a href={"#section4"}>Contact</a></div>
         </div>
     )
 };
