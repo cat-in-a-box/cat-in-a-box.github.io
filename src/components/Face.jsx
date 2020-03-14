2f264aa40f7094d6ae8d20decbfd705f97bad995
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-03-14 17:49:43 +0200

    Changed pics paths

--- src/components/Face.jsx
+++ src/components/Face.jsx
@@ -6,7 +6,7 @@ const Face = () => {
             <h1>Hello!</h1>
             <h1>I am Alexander</h1>
             <h2>Beginner React developer</h2>
-            <img className="reactIcon" img src={"react-icon.png"} alt="react-icon" height="64" width="64"/>
+            <img className="reactIcon" src={"pics/react-icon.png"} alt="react-icon" height="64" width="64"/>
         </div>
     )
 };
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,32 +7,32 @@ const Works = () => {
             <div className="Works"><h1>My Works</h1>
                 <div className="WorksPlaceholder">
                     <a href="https://cat-in-a-box.github.io/mousewheel_checker/" className="Card">
-                        <img src={"mouse.png"} alt="MouseIcon"/>
-                        <img src={"mouse2.png"} alt="MouseIcon2"/>
+                        <img src={"pics/mouse.png"} alt="MouseIcon"/>
+                        <img src={"pics/mouse2.png"} alt="MouseIcon2"/>
                         <div className="CardContainer">
                             <h3><b>Mousewheel Checker</b></h3>
                             <h4>The app to check your mouse wheel</h4>
                         </div>
                     </a>
                     <a href="https://cat-in-a-box.github.io/calculator" className="Card">
-                        <img src={"calculator.png"} alt="CaloriesIcon"/>
-                        <img src={"calculator2.png"} alt="CaloriesIcon2"/>
+                        <img src={"pics/calculator.png"} alt="CaloriesIcon"/>
+                        <img src={"pics/calculator2.png"} alt="CaloriesIcon2"/>
                         <div className="CardContainer">
                             <h3><b>Calculator</b></h3>
                             <h4>Just a simple calculator to do your math</h4>
                         </div>
                     </a>
                     <a href="https://cat-in-a-box.github.io/gallery-react/" className="Card">
-                        <img src={"paw.png"} alt="PawIcon" width="150px"/>
-                        <img src={"paw2.png"} alt="PawIcon2" width="150px"/>
+                        <img src={"pics/paw.png"} alt="PawIcon" width="150px"/>
+                        <img src={"pics/paw2.png"} alt="PawIcon2" width="150px"/>
                         <div className="CardContainer">
                             <h3><b>Animal Gallery</b></h3>
                             <h4>Check these beautiful pictures of animals</h4>
                         </div>
                     </a>
                     <a href="https://cat-in-a-box.github.io/snake-game/" className="Card">
-                        <img src={"chicken.png"} alt="ChickenIcon" width="180px"/>
-                        <img src={"chicken2.png"} alt="ChickenIcon2" width="180px"/>
+                        <img src={"pics/snake.png"} alt="ChickenIcon" width="180px"/>
+                        <img src={"pics/snake2.png"} alt="ChickenIcon2" width="180px"/>
                         <div className="CardContainer">
                             <h3><b>Snake!!!</b></h3>
                             <h4>Super Zmeika!!! Just move it and eat food</h4>
