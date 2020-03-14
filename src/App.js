a633887d833df7efc37f67ba1e7540956f632124
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-03-14 16:45:59 +0200

    Removed Helmet package

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 // Установил и импортировал Helmet, с помощью которого изменил page title.
 // Насколько я понял, это самый простой способ менять его (как и атрибуты страницы) на реакте.
 // А еще подгружает скрипты если что вдруг. Нормально.
+// Потом убрал его, нашел другой способ изменять тайтл.
 // Поставил еще React-Scrollable-Anchor для плавного перемещения по якорям
 
-import {Helmet} from "react-helmet";
 import Header from './components/Header.jsx';
 import Face from "./components/Face.jsx";
 import About from "./components/About.jsx";
@@ -18,12 +18,6 @@ import Contact from "./components/Contact.jsx";
 function App() {
     return (
         <div className="App">
-            <Helmet>
-                <title>Alexander Timoshuk</title>
-                <meta charSet="utf-8"/>
-                <link rel="preload" href="App.css" as="style"/>
-                {/*прелоад стилей, шрифтов, вот это всё. Чёт вроде не работает*/}
-            </Helmet>
             <Header/>
             <Face/>
             <About/>
