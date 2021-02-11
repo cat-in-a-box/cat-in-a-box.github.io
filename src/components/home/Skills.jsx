f7eaca2f44c48b16ebf0df40f0f0d46dc694d657
cat-in-a-box <timoshuk.alexander@gmail.com>
2021-02-11 19:19:16 +0300

    more skills added

--- src/components/home/Skills.jsx
+++ src/components/home/Skills.jsx
@@ -5,28 +5,36 @@ const Skills = () => {
         <div className="Skills"><h1>Мои навыки</h1>
             <div className="SkillsIconPlaceHolder">
                 <div className="SkillsIcon">
-                    <img src={require('./files/pics/code.png')} alt="HTML и CSS" width="50px"/>
+                    <img src={require('./files/pics/code.png')} alt="HTML-CSS"/>
                     <div className="Popup">HTML и CSS</div>
                 </div>
                 <div className="SkillsIcon">
-                    <img src={require('./files/pics/javascript.png')} alt="JavaScript" width="50px"/>
+                    <img src={require('./files/pics/javascript.png')} alt="JavaScript"/>
                     <div className="Popup">JavaScript</div>
                 </div>
                 <div className="SkillsIcon">
-                    <img src={require('./files/pics/react.png')} alt="React" width="50px"/>
+                    <img src={require('./files/pics/react.png')} alt="React"/>
                     <div className="Popup">React</div>
                 </div>
                 <div className="SkillsIcon">
-                    <img src={require('./files/pics/responsive.png')} alt="Адаптивность" width="50px"/>
+                    <img src={require('./files/pics/responsive.png')} alt="adaptive"/>
                     <div className="Popup">Адаптивность</div>
                 </div>
                 <div className="SkillsIcon">
-                    <img src={require('./files/pics/sass.png')} alt="Препроцессоры" width="50px"/>
+                    <img src={require('./files/pics/sass.png')} alt="Preprocessors"/>
                     <div className="Popup">Sass, SCSS, Less</div>
                 </div>
                 <div className="SkillsIcon">
-                    <img src={require('./files/pics/bootstrap.png')} alt="Bootstrap" width="50px"/>
+                    <img src={require('./files/pics/bootstrap.png')} alt="Bootstrap"/>
                     <div className="Popup">Bootstrap</div>
+                </div>
+                <div className="SkillsIcon">
+                    <img src={require('./files/pics/git.png')} alt="Git"/>
+                    <div className="Popup">Git</div>
+                </div>
+                <div className="SkillsIcon">
+                    <img src={require('./files/pics/next.png')} alt="Next"/>
+                    <div className="Popup">Next.js</div>
                 </div>
             </div>
         </div>
