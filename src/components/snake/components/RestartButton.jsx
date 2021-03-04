bb6b59db479f73b9dc07668c351430a0f4e4ec9b
cat-in-a-box <timoshuk.alexander@gmail.com>
2021-03-04 20:02:25 +0300

    removed unused code

--- src/components/snake/components/RestartButton.jsx
+++ src/components/snake/components/RestartButton.jsx
@@ -1,20 +1,6 @@
 import React from "react";
 
-function RestartButton(event) {
-
-    //Листенер сидит и жмет пока ты нажмешь на кнопку. Нужен чтобы детектить это дело, иначе кнопка не проходит
-    document.addEventListener("keydown", RestartButton);
-
-    //Назначение кода на пробел
-    let spaceBar = 32;
-    let spaceBarPressed = event.keyCode;
-
-    //Перезагрузка страницы
-    if (spaceBarPressed === spaceBar) {
-        window.location.reload();
-    }
-
-    //Собственно самам кнопка
+function RestartButton() {
     return (
         <a className={"RestartButton"} href={"https://cat-in-a-box.github.io/snake"}>Перезапуск (Пробел)</a>
     )
