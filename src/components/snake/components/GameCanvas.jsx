7d8ccf6f0c939f9678324d64fd4f4672f08b1774
cat-in-a-box <timoshuk.alexander@gmail.com>
2021-03-04 20:02:46 +0300

    small fix

--- src/components/snake/components/GameCanvas.jsx
+++ src/components/snake/components/GameCanvas.jsx
@@ -6,7 +6,7 @@ const GameCanvas = () => {
             <div>Съедено колбасок:</div>
             <div className={"score"} id="score">0</div>
             <div id={'scoreUnderText'} className={"scoreUndertext"}>Двигай змейку с помощью стрелок на клавиатуре</div>
-            <canvas className={"GameField"} id={"gameCanvas"} width={"810"} height={"510"}/>
+            <canvas id={"gameCanvas"} className={"GameField"} width={"810"} height={"510"}/>
         </div>
     )
 };
