a86e62a895e39e6f2087e9477bd374d999150325
cat-in-a-box <timoshuk.alexander@gmail.com>
2021-03-04 20:08:26 +0300

    old pics preload removed

--- src/components/mousewheelChecker/components/WheelCheckArea.jsx
+++ src/components/mousewheelChecker/components/WheelCheckArea.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const WheelCheckArea = () => {
     return (
         <div className={"WheelCheckArea"}>
-            <p id={"Wheel"}/>
-            <div className={"WheelPreload1"}/>
-            <div className={"WheelPreload2"}/>
+            <div id={"Wheel"}/>
         </div>
     )
 }
