219c1f257832bd4aaeb646f0603f4589aef6be8c
cat-in-a-box <timoshuk.alexander@gmail.com>
2021-03-07 21:25:28 +0300

    link to github repo added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className={"Footer"}>
             <p>Сделано с душой и используя чай в качестве топлива.</p>
-            <p>updated: March 2021</p>
+            <p><a href={'https://github.com/cat-in-a-box/cat-in-a-box.github.io'} target={'_blank'} rel="noopener noreferrer">GitHub</a> / updated: March 2021</p>
         </div>
     )
 };
