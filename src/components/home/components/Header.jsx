155f2c9e2ac07c74febd6e9b772f90f7fae26ab2
cat-in-a-box <timoshuk.alexander@gmail.com>
2021-03-09 00:41:57 +0300

    small header fix

--- src/components/home/components/Header.jsx
+++ src/components/home/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Header = () => {
 	return (
 			<header id={'Header'}>
-				<div className={'HeaderButton'}><a href={'#face'}>Главная</a></div>
-				<div className={'HeaderButton'}><a href={'#about'}>Обо мне</a></div>
-				<div className={'HeaderButton'}><a href={'#projects'}>Проекты</a></div>
-				<div className={'HeaderButton'}><a href={'#contacts'}>Контакты</a></div>
+				<div className={'HeaderButton'}><a href={'#Face'}>Главная</a></div>
+				<div className={'HeaderButton'}><a href={'#About'}>Обо мне</a></div>
+				<div className={'HeaderButton'}><a href={'#Works'}>Проекты</a></div>
+				<div className={'HeaderButton'}><a href={'#Contacts'}>Контакты</a></div>
 				<div className={'HeaderButton'}><a href={'https://github.com/cat-in-a-box'} target={'blank'}>GitHub</a></div>
 			</header>
 	)
