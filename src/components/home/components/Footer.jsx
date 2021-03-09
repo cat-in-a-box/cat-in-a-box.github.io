b5b3d52a0f75218d0fce8d9fe33aad41193d55bf
cat-in-a-box <timoshuk.alexander@gmail.com>
2021-03-10 02:47:51 +0300

    small text changes

--- src/components/home/components/Footer.jsx
+++ src/components/home/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <footer id={"Footer"}>
-            <p>Сделано с душой и используя чай в качестве топлива.</p>
+            <p>Сделано на React с душой и используя чай в качестве топлива.</p>
             <p><a href={'https://github.com/cat-in-a-box/cat-in-a-box.github.io'} target={'_blank'} rel="noopener noreferrer">GitHub</a> / updated: March 2021</p>
         </footer>
     )
