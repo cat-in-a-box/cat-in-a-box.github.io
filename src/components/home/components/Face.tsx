b5295231ca9f213fea3f844691bdd72e9c499ca3
cat-in-a-box <timoshuk.alexander@gmail.com>
2021-07-12 20:51:05 +0300

    few fixes

--- src/components/home/components/Face.tsx
+++ src/components/home/components/Face.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Face = () => {
-	return (
-			<div id ={'Face'}>
-				<img className={'faceImg'} src={require('../files/pics/face.jpg')} alt={'face'}/>
-				<h1>Меня зовут Александр Тимощук</h1>
-				<h2>Front End разработчик</h2>
-			</div>
-	)
+    return (
+        <div id={'Face'}>
+            <img className={'faceImg'} src={require('../files/pics/face.jpg')} alt={'face'}/>
+            <h1>Меня зовут Александр Тимощук</h1>
+            <h2>Front End разработчик</h2>
+        </div>
+    )
 }
 
 export default Face
