9f5b58d6a7cce0e1c6cc8e1dbcba31e861eb02b0
cat-in-a-box <timoshuk.alexander@gmail.com>
2021-07-12 19:48:02 +0300

    small fixes

--- src/components/home/components/Header.tsx
+++ src/components/home/components/Header.tsx
@@ -1,48 +1,48 @@
-import React, { Component } from 'react'
-import { HashLink } from 'react-router-hash-link'
+import React, {Component} from 'react'
+import {HashLink} from 'react-router-hash-link'
 import DarkModeButton from './DarkModeButton'
 
 export default class PreloaderRemover extends Component {
-	componentDidMount() {
-		let navbar = document.getElementById('Header') as HTMLElement;
-		let toggleBackground = document.getElementById('ToggleBackground') as HTMLElement;
-		let toggleButton = document.getElementById('ToggleButton') as HTMLElement;
-		let sticky:number = navbar.offsetTop + 60;
+    componentDidMount() {
+        let navbar = document.getElementById('Header') as HTMLElement;
+        let toggleBackground = document.getElementById('ToggleBackground') as HTMLElement;
+        let toggleButton = document.getElementById('ToggleButton') as HTMLElement;
+        let sticky: number = navbar.offsetTop + 60;
 
-		window.onscroll = function () {
-			navbarFixing()
-		}
+        window.onscroll = function () {
+            navbarFixing()
+        }
 
-		function navbarFixing() {
-			if (window.pageYOffset >= sticky) {
-				navbar.classList.add('HeaderFixed')
-				toggleButton.classList.add('ToggleButtonFixed')
-				toggleBackground.classList.add('ToggleBackgroundFixed')
-			} else {
-				navbar.classList.remove('HeaderFixed')
-				toggleButton.classList.remove('ToggleButtonFixed')
-				toggleBackground.classList.remove('ToggleBackgroundFixed')
-			}
-		}
-	}
+        function navbarFixing() {
+            if (window.pageYOffset >= sticky) {
+                navbar.classList.add('HeaderFixed')
+                toggleButton.classList.add('ToggleButtonFixed')
+                toggleBackground.classList.add('ToggleBackgroundFixed')
+            } else {
+                navbar.classList.remove('HeaderFixed')
+                toggleButton.classList.remove('ToggleButtonFixed')
+                toggleBackground.classList.remove('ToggleBackgroundFixed')
+            }
+        }
+    }
 
-	render() {
-		return (
-				<header id={'Header'}>
-					<div className={'HeaderButton'}>
-						<HashLink to={'/#About'}>Обо мне</HashLink>
-					</div>
-					<div className={'HeaderButton'}>
-						<HashLink to={'/#Works'}>Проекты</HashLink>
-					</div>
-					<div className={'HeaderButton'}>
-						<HashLink to={'/#Contacts'}>Контакты</HashLink>
-					</div>
-					<div className={'HeaderButton'}>
-						<a href={'https://github.com/cat-in-a-box'} target={'blank'}>GitHub</a>
-					</div>
-					<DarkModeButton/>
-				</header>
-		)
-	}
+    render() {
+        return (
+            <header id={'Header'}>
+                <div className={'HeaderButton'}>
+                    <HashLink to={'/#About'}>Обо мне</HashLink>
+                </div>
+                <div className={'HeaderButton'}>
+                    <HashLink to={'/#Works'}>Проекты</HashLink>
+                </div>
+                <div className={'HeaderButton'}>
+                    <HashLink to={'/#Contacts'}>Контакты</HashLink>
+                </div>
+                <div className={'HeaderButton'}>
+                    <a href={'https://github.com/cat-in-a-box'} target={'blank'}>GitHub</a>
+                </div>
+                <DarkModeButton/>
+            </header>
+        )
+    }
 }
