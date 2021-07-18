c65a65b6fed751533bc0e5b3782a259bd2baddde
cat-in-a-box <timoshuk.alexander@gmail.com>
2021-07-19 00:05:03 +0300

    some protection for email harvesing

--- src/components/home/components/Contact.tsx
+++ src/components/home/components/Contact.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
 function Contact() {
-	return (
-			<div id={'Contacts'} className={'Contact'}>
-				<h1>Свяжитесь со мной!</h1>
-				<p>Просто напишите письмо на</p>
-				<p className={'FooterEmail'}>timoshuk.alexander@gmail.com</p>
-			</div>
-	)
+    return (
+        <div id={'Contacts'} className={'Contact'}>
+            <h1>Свяжитесь со мной!</h1>
+            <p>Просто напишите письмо на</p>
+			{/*Implemented some simple protection against email harvesting*/}
+            <p className={'FooterEmail'}>
+                timo
+                <span className={'def'}>bou</span>
+                shuk.ale
+                <span className={'def'}>eno</span>
+                xander@gm
+                <span className={'def'}>fam</span>
+                ail.com
+            </p>
+
+        </div>
+    )
 }
 
 export default Contact
